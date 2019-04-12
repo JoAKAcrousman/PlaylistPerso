0c23a92616db604f3af71434cda3441c4d9c646b
Elisa <ciavaldini.e@gmail.com>
2019-04-12 12:44:22 +0200

    Page accueil
    Création page accueil, gif, titre

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="./images/icon.png">
+
+	<title>MELE'OHANA</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+	<section class="one-page">
+		<h1 class="title">ME<span>LE'OHA</span>NA</h1>
+		<img class="gif-bg" src="images/gifback.gif" width="450" alt="">
+	</section>
+</body>
+
+</html>
