0134b160265cd511c5ac333e6ed50f6fc486b7d6
Unknown <veysset.sarah@gmail.com>
2019-05-12 12:19:48 +0200

    [BACK] Fichier pour inclure la BDD
    Ajout pour commenter/décommenter rapidement

--- api/MyPDO.elisaciaks9.include.php
+++ api/MyPDO.elisaciaks9.include.php
@@ -3,4 +3,10 @@
 require_once 'MyPDO.class.php';
 
 
+
+/* 
 MyPDO::setConfiguration('mysql:host=elisaciaks9.mysql.db;dbname=elisaciaks9;charset=utf8', 'elisaciaks9', 'FzvcDTgxzFkHe9v');
+*/
+
+//localhost
+MyPDO::setConfiguration('mysql:host=localhost;dbname=elisaciaks9;charset=utf8', 'root', 'root');
